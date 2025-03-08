96337ad506851bab47dbd91a19dc0630c8a09952
Sinjini Sarkar <146257704+sinjinisarkar@users.noreply.github.com>
2025-03-08 18:39:55 +0000

    Added email configuration inside config.py

--- config.py
+++ config.py
@@ -1,6 +1,5 @@
 import os 
 
-
 basedir = os.path.abspath(os.path.dirname(__file__))
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
 SQLALCHEMY_TRACK_MODIFICATIONS = True
@@ -8,3 +7,11 @@ SQLALCHEMY_TRACK_MODIFICATIONS = True
 WTF_CSRF_ENABLED = True
 SECRET_KEY = 'a-very-secret-secret'
 
+# Email Configuration
+MAIL_SERVER = 'smtp.gmail.com'
+MAIL_PORT = 587
+MAIL_USE_TLS = True
+MAIL_USE_SSL = False
+MAIL_USERNAME = 'no.reply.catchmyride@gmail.com'
+MAIL_PASSWORD = 'yzvr gzdc tywg kysg' # App Password
+MAIL_DEFAULT_SENDER = 'no.reply.catchmyride@gmail.com'
