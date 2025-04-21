0fa2294c89e0a0243910cab8628026a609cd4860
Sinjini Sarkar <146257704+sinjinisarkar@users.noreply.github.com>
2025-04-21 16:05:21 +0000

    Fix: prevent chat polling for unauthenticated users

--- app/views.py
+++ app/views.py
@@ -1338,6 +1338,8 @@ def get_commute_live_locations(ride_id, ride_date):
 
 
 # ID 17: chat option between driver and passenger
+
+# route to check if the user is not logged in for chat polling
 @app.route('/api/is_logged_in')
 def is_logged_in():
     return jsonify({"logged_in": current_user.is_authenticated})
